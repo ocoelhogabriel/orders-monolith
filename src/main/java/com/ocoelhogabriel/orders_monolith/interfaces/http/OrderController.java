e68203c3591a2b4f28c1b67354851dafca7cbbb8
Gabriel Coelho <gabriel.coelho.hgc@outlook.com.br>
2025-10-06 11:56:39 -0300

    Sync

--- src/main/java/com/ocoelhogabriel/orders_monolith/interfaces/http/OrderController.java
+++ src/main/java/com/ocoelhogabriel/orders_monolith/interfaces/http/OrderController.java
@@ -3,8 +3,13 @@ package com.ocoelhogabriel.orders_monolith.interfaces.http;
 import java.util.UUID;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ocoelhogabriel.orders_monolith.application.dto.CreateOrderDto;
 import com.ocoelhogabriel.orders_monolith.application.dto.OrderView;
